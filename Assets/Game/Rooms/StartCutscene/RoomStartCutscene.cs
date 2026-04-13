47ba927129eba8de60878164dde9710a34e9b48b
fr0g-z <martinafarrugia6@gmail.com>
2026-04-13 13:45:52 +0200

    Cutscene fix
    Cutscene turned into an animation on unity, script added so now cutscene cant be skipped and player is sent to bedroom after cutscene

--- Assets/Game/Rooms/StartCutscene/RoomStartCutscene.cs
+++ Assets/Game/Rooms/StartCutscene/RoomStartCutscene.cs
@@ -3,29 +3,20 @@ using System.Collections;
 using PowerTools.Quest;
 using PowerScript;
 using static GlobalScript;
-using UnityEngine.Video;
-using UnityEngine.SceneManagement;
 
 public class RoomStartCutscene : RoomScript<RoomStartCutscene>
 {
 
-    VideoPlayer video;
-    
-    IEnumerator OnEnterRoomAfterFade()
-	{
-        Cursor.Visible = true;
-        video = GameObject.Find("Intro").GetComponent<VideoPlayer>();
-        video.Play();
-       
-        yield return E.Break;
-
-    }
-    
     IEnumerator OnInteractHotspotContinue( IHotspot hotspot )
 	{
         yield return E.ChangeRoom(R.Bedroom);
         yield return E.Break;
 	}
 
-	
+
+    IEnumerator OnEnterRoomAfterFade()
+    {
+        yield return new WaitForSeconds(31.5f);
+        yield return E.ChangeRoom(R.Bedroom);
+    }
 }
